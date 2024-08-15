48192c5fbff7da7a7a879054c98bac67013dc5bd
HarithaMihimal <harithamihimal4@gmail.com>
2024-08-15 17:27:56 +0530

    Refactor main.js to remove commented out code, improve variable naming, and add error and warning logs

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,7 +1,14 @@
-console.log('hi')
-let xname=prompt('Enter your name','Anonymous')
-let yname = confirm('are you haritha')
-console.log(yname)
+// console.log('hi')
+// let xname=prompt('Enter your name','Anonymous')
+// let yname = confirm('are you haritha')
+// console.log(yname)
 
-console.error('This is an error')
-console.warn('This is a warning')
+// console.error('This is an error')
+// console.warn('This is a warning')
+
+
+let a ={
+    name: 'haritha',
+    age: 22,
+    city: 'Matara'
+}
