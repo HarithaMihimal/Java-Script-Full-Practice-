db2aac0575a1de3122d542d637d7bc1c78257018
HarithaMihimal <harithamihimal4@gmail.com>
2024-08-15 22:39:13 +0530

    Refactor main.js to improve variable naming and add new object

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,24 +1,44 @@
-// console.log('hi')
-// let xname=prompt('Enter your name','Anonymous')
-// let yname = confirm('are you haritha')
-// console.log(yname)
+// // console.log('hi')
+// // let xname=prompt('Enter your name','Anonymous')
+// // let yname = confirm('are you haritha')
+// // console.log(yname)
 
-// console.error('This is an error')
-// console.warn('This is a warning')
+// // console.error('This is an error')
+// // console.warn('This is a warning')
 
 
-let a ={
-    name: 'haritha',
-    age: 22,
-    city: 'Matara'
-}
+// let a ={
+//     name: 'haritha',
+//     age: 22,
+//     city: 'Matara'
+// }
 
-let b ={
-    name: 'mihimal',
-    age: 22,
-    city: 'Matara'
-}
+// let b ={
+//     name: 'mihimal',
+//     age: 22,
+//     city: 'Matara'
+// }
 
-console.log(a)
-console.log(b)
-console.table({a,b})
+// console.log(a)
+// console.log(b)
+// console.table({a,b})
+
+
+let a="haritha Mihimal WIlwala "
+let b=23
+let c=true
+let d=null
+let e=undefined
+let f=[1,2,3,4,5]
+let g={name:'haritha',age:22,city:'Matara'}
+let h=new Date()
+
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f)
+console.log(typeof g)
+console.log(typeof h)
