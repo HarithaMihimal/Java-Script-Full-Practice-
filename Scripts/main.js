fe5cb6a5b5205f5e0aca227d6a256e6a1ae575e5
HarithaMihimal <harithamihimal4@gmail.com>
2024-08-29 09:44:27 +0530

    Refactor main.js to optimize string manipulation methods and add new object

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,102 +1,126 @@
-// // console.log('hi')
-// // let xname=prompt('Enter your name','Anonymous')
-// // let yname = confirm('are you haritha')
-// // console.log(yname)
+// // // console.log('hi')
+// // // let xname=prompt('Enter your name','Anonymous')
+// // // let yname = confirm('are you haritha')
+// // // console.log(yname)
 
-// // console.error('This is an error')
-// // console.warn('This is a warning')
+// // // console.error('This is an error')
+// // // console.warn('This is a warning')
 
 
-// // let a ={
-// //     name: 'haritha',
-// //     age: 22,
-// //     city: 'Matara'
-// // }
+// // // let a ={
+// // //     name: 'haritha',
+// // //     age: 22,
+// // //     city: 'Matara'
+// // // }
 
-// // let b ={
-// //     name: 'mihimal',
-// //     age: 22,
-// //     city: 'Matara'
-// // }
+// // // let b ={
+// // //     name: 'mihimal',
+// // //     age: 22,
+// // //     city: 'Matara'
+// // // }
 
-// // console.log(a)
+// // // console.log(a)
+// // // console.log(b)
+// // // console.table({a,b})
+
+
+// // let a="haritha Mihimal WIlwala "
+// // let b=23
+// // let c=true
+// // let d=null
+// // let e=undefined
+// // let f=[1,2,3,4,5]
+// // let g={name:'haritha',age:22,city:'Matara'}
+// // let h=new Date()
+
+
+// // // console.log(typeof a)
+// // // console.log(typeof b)
+// // // console.log(typeof c)
+// // // console.log(typeof d)
+// // // console.log(typeof e)
+// // // console.log(typeof f)
+// // // console.log(typeof g)
+// // // console.log(typeof h)
+
+// // b=String(b)
 // // console.log(b)
-// // console.table({a,b})
-
-
-// let a="haritha Mihimal WIlwala "
-// let b=23
-// let c=true
-// let d=null
-// let e=undefined
-// let f=[1,2,3,4,5]
-// let g={name:'haritha',age:22,city:'Matara'}
-// let h=new Date()
-
-
-// // console.log(typeof a)
 // // console.log(typeof b)
-// // console.log(typeof c)
-// // console.log(typeof d)
-// // console.log(typeof e)
-// // console.log(typeof f)
-// // console.log(typeof g)
-// // console.log(typeof h)
-
-// b=String(b)
-// console.log(b)
-// console.log(typeof b)
-// let i='100.33E'
-// console.log(Number(b))
-// console.log((parseInt(i)))
+// // let i='100.33E'
+// // console.log(Number(b))
+// // console.log((parseInt(i)))
 
 
-//unary operator
+// //unary operator
 
-// let a=2+3
-// console.log(a)
-// let c='4'
+// // let a=2+3
+// // console.log(a)
+// // let c='4'
 
-// console.log(+c+10) 
+// // console.log(+c+10) 
 
-// age=24
+// // age=24
 
-// console.log(`your age is ${age}`)
-// const myname='haritha'
-// console.log(myname)
-// myname='mihimal'
+// // console.log(`your age is ${age}`)
+// // const myname='haritha'
+// // console.log(myname)
+// // myname='mihimal'
 
-// let a,b,c;
-// a=b=c=20;
-// console.log(a,b,c)
+// // let a,b,c;
+// // a=b=c=20;
+// // console.log(a,b,c)
 
-// let a='haritha'
-// console.log(a[0])
+// // let a='haritha'
+// // console.log(a[0])
 
-// let a='haritha'
-// console.log(a.split('r'))
-// let a='haritha'
-// console.log(a.charAt(4))
+// // let a='haritha'
+// // console.log(a.split('r'))
+// // let a='haritha'
+// // console.log(a.charAt(4))
 
-// let a='haritha'
-// console.log(a.replace('a','A'))
+// // let a='haritha'
+// // console.log(a.replace('a','A'))
 
-// let a='Haritha'
-// console.log(a.includes('r'))
-// console.log(a.includes('z'))
-// console.log(a.lastIndexOf('a'))
-// console.log(a.indexOf('a'))
-// console.log(a.toUpperCase())
-// console.log(a.toLowerCase())
-// console.log(a.substring(0,3))
-// console.log(a.slice(0,3))
-// console.log(a.slice(-3))
-// console.log(a.slice(0,-3))
-// console.log(a.split('a'))
-// console.log(a.split(''))
+// // let a='Haritha'
+// // console.log(a.includes('r'))
+// // console.log(a.includes('z'))
+// // console.log(a.lastIndexOf('a'))
+// // console.log(a.indexOf('a'))
+// // console.log(a.toUpperCase())
+// // console.log(a.toLowerCase())
+// // console.log(a.substring(0,3))
+// // console.log(a.slice(0,3))
+// // console.log(a.slice(-3))
+// // console.log(a.slice(0,-3))
+// // console.log(a.split('a'))
+// // console.log(a.split(''))
 
 
-let d=['haritha','mihimal','wilwala',10,true,undefined]
-console.log(d)
-console.log(d.length)
+// let d=['haritha','mihimal','wilwala',10,true,undefined]
+// console.log(d)
+// console.log(d.length)
+// console.log(Array.isArray(d))
+// console.log(d.indexOf('mihimal'))
+// console.log(d.indexOf('z'))
+// console.log(d.push('kumara'))
+// console.log(d)
+// console.log(d.pop())
+// console.log(d.reverse())
+// console.log(d.concat(['kumara','kumara']))
+// console.log(d.join(' '))
 
+
+let a={
+    name:"Amila Upul",
+    age:22,
+    city:"Matara",
+    canFly:true,
+    languages:['sinhala','english','tamil'],
+    city1:{
+        name:'Matara',
+        population:200000
+    },
+    
+
+}
+console.log(a)
